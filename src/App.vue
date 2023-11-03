76861695cb9a88c8aba4c673f6c4ca750d5a2fa6
kokotyan <kokochi.0207@gmail.com>
2023-11-03 10:54:20 +0900

    クレジット画面を追加しました

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/new">New</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">Credit</router-link>
   </nav>
   <router-view/>
 </template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="credit">
+    <h1>このソフトを作った人</h1>
+    <p>制作者: 爰地祐哉</p>
+    <p>制作日: 2023/10/31</p>
+    <p>バージョン: 1.0</p>
+    <div>
+      <a href="https://twitter.com/murakumo_yu">
+        <i class="fab fa-twitter"></i> 
+      </a>
+      <a href="https://github.com/kokotyan">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.credit {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.social-icons {
+  display: flex;
+  gap: 10px; 
+}
+</style>
